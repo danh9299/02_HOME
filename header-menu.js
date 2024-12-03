78a783cab592ce8e0075b3ed25737bb1505c8c68
anhnd1 <anhnd1@vietis.com.vn>
2024-12-03 11:20:47 +0700

    push code

--- header-menu.js
+++ header-menu.js
@@ -1,12 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const headerMenu = document.querySelector(".Header__menu");
   const headerNav = document.querySelector(".Header-nav");
+  const headerClose = document.querySelector(".Header__close");
+  const headerModal = document.querySelector(".Header__modal");
 
   headerMenu.addEventListener("click", () => {
-    if (headerNav.style.display === "flex") {
-      headerNav.style.display = "none";
-    } else {
-      headerNav.style.display = "flex";
-    }
+    headerMenu.classList.toggle("Header__close");
+    headerNav.classList.toggle("Header-nav--show");
+    headerModal.classList.toggle("Header__modal--show");
+  });
+
+  headerModal.addEventListener("click", () => {
+    headerMenu.classList.toggle("Header__close");
+    headerNav.classList.toggle("Header-nav--show");
+    headerModal.classList.toggle("Header__modal--show");
   });
 });
--- open-image.js
+++ open-image.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const workRowItems = document.querySelectorAll(".Work-row-item");
   const workModal = document.querySelector(".Work-modal");
+  const workModalContent = document.querySelector(".Work-content");
   const workModalClose = document.querySelector(".Work-modal__close");
 
   workRowItems.forEach((item) => {
@@ -8,12 +9,19 @@ document.addEventListener("DOMContentLoaded", () => {
     item.addEventListener("click", () => {
       if (workModal.style.display != "block") {
         workModal.style.display = "block";
-        workModal.style.backgroundColor = initialBgColor;
+        workModalContent.style.display = "block";
+        workModalContent.style.backgroundColor = initialBgColor;
       }
     });
   });
 
   workModalClose.addEventListener("click", () => {
     workModal.style.display = "none";
+    workModalContent.style.display = "none";
+  });
+
+  workModal.addEventListener("click", () => {
+    workModal.style.display = "none";
+    workModalContent.style.display = "none";
   });
 });
